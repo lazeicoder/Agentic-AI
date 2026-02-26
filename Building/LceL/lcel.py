b0695d862b3a6336b174b380ee113a185df34390
Anurag Mukherjee <anuragmukherjeedev@gmail.com>
2026-02-26 20:09:07 +0530

    New Commit

--- Building/LceL/lcel.py
+++ Building/LceL/lcel.py
@@ -1,14 +1,46 @@
-import os 
-import streamlit as st
-
-from dotenv import load_dotenv
-from langchain_community.llms import Ollama
+from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["GROK_API_KEY"] = os.getenv("GROK_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+groq_api_key = os.getenv("GROK_API_KEY")
+model = ChatGroq(model="llama-3.1-8b-instant", groq_api_key=groq_api_key) 
+
+
+system_template = "You are an expert AI Engineer with over 30+ years of experience in the Software and AI Research domain, with large scale understanding of how AI (different machine learning, deep learning, reinforcement learning techniques, transformers, agentic ai) systems work, perform, architecture, and how to scale them for larger client and user base. Based on your experience in the field, provide me with answers mathematically."
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template),
+        ("user", "{text}"),
+    ]
+)
+
+# Create output parser
+parser = StrOutputParser()
+
+# Create chain
+chain = prompt_template | model | parser 
+
+# App Defination
+app = FastAPI(
+    title="Langchain Server", 
+    version="1.0",
+    description="A simple API server using Langchain"
+)
+
+# Adding chain Routes
+add_routes(
+    app, 
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8080)
